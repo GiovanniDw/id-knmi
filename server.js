2b468a2332fb0ef673b38b7829bb315f1f4c5302
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2021-01-06 13:12:16 +0100

    private ket name

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 require('dotenv').config();
 
+const ee = require('@google/earthengine');
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 5000;
+const privateKey = require(`./${process.env.PRIVATE_KEY}`);
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+console.log(privateKey.private_key);
 
-console.log(PRIVATE_KEY);
+var runAnalysis = function () {
+	ee.initialize(
+		null,
+		null,
+		function () {
+			// ... run analysis ...
+		},
+		function (e) {
+			console.error('Initialization error: ' + e);
+		}
+	);
+};
 
+ee.data.authenticateViaPrivateKey(
+	privateKey.private_key,
+	runAnalysis,
+	function (e) {
+		console.error('Authentication error: ' + e);
+	}
+);
+
+var image = new ee.Image('srtm90_v4');
+image.getMap({ min: 0, max: 1000 }, function (map) {
+	console.log(map);
+});
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_server', (req, res) => {
 	res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+const google = 'google';
+
+app.get('/earth_engine', (req, res) => {
+	res.send({ google: image });
 });
