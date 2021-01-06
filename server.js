0dcf45d2928435d8aa2b9f87aea41f10af2e9ff3
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2021-01-06 11:23:24 +0100

    server update

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+console.log(PRIVATE_KEY);
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
