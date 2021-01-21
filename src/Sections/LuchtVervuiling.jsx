bf71f880bd50831c413409b19a6518c0fa7cc3da
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2021-01-21 12:49:13 +0100

    update

--- src/Sections/LuchtVervuiling.jsx
+++ src/Sections/LuchtVervuiling.jsx
@@ -15,7 +15,7 @@ import {
 
 import { UitstootChart } from '../Components/svg';
 
-const LuchtVervuiling = (props) => {
+export const LuchtVervuiling = (props) => {
 	const { activeCountry } = props;
 
 	const [isInViewport, useTriggerRef] = useIsInViewport();
@@ -42,44 +42,46 @@ const LuchtVervuiling = (props) => {
 	}, [sixthHeadingRef]);
 
 	return (
+		<SmallSection>
 			<FlexContainer>
+				<SectionTitle alignSelf="flex-end">
+					<h2 ref={sixthHeadingRef}>
+						Hoe zit het met luchtvervuiling <br />
 						in {activeCountry ? activeCountry.name : 'Nederland'}
 					</h2>
 				</SectionTitle>
 				<FlexItem flexGrow="1" alignItems="stretch">
 					<FlexContainer
+						ref={useTriggerRef}
 						flexDirection="row"
 						justifyContent="space-around"
 						alignItems="stretch"
 						alignContent="center"
-						ref={useTriggerRef}
 					>
-						<>
-							<FlexItem
-								flexGrow="1"
-								justifyContent="center"
-								alignSelf="center"
-							>
-								<img src={activeCountry.tropomi} />
-							</FlexItem>
-							<FlexItem
-								flexGrow="1"
-								alignSelf="stretch"
-								justifyContent="center"
-							>
-								<>
-									<UitstootChart
-										height="100%"
-										width="100%"
-										uitstoot={activeCountry.uitstoot}
-									/>
-								</>
-							</FlexItem>
-						</>
+						<FlexItem
+							flexGrow="1"
+							justifyContent="center"
+							alignSelf="center"
+						>
+							<img src={activeCountry.tropomi} />
+						</FlexItem>
+
+						<FlexItem
+							flexGrow="1"
+							alignSelf="stretch"
+							justifyContent="center"
+						>
+							<UitstootChart
+								isInViewport={isInViewport}
+								height="100%"
+								width="100%"
+								uitstoot={activeCountry.uitstoot}
+							/>
+						</FlexItem>
 					</FlexContainer>
 				</FlexItem>
 			</FlexContainer>
-		</Section>
+		</SmallSection>
 	);
 };
 
